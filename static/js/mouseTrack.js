4bd47851aef0c62fbccb841362f4bd467968f533
spooketti <jonliu168@gmail.com>
2024-09-12 15:06:50 -0700

    space to start/stop

--- static/js/mouseTrack.js
+++ static/js/mouseTrack.js
@@ -1,32 +1,43 @@
-window.setInterval(pushTime,50)
+let recordInterval = window.setInterval(pushTime, 50)
 let x = 0
 let y = 0
-document.addEventListener("mousemove",function(e)
-{
-    x = e.clientX - (window.innerWidth/2)
-    y = (window.innerHeight/2)-e.clientY
+let isRecording = true
+document.addEventListener("mousemove", function (e) {
+    x = e.clientX - (window.innerWidth / 2)
+    y = (window.innerHeight / 2) - e.clientY
 })
 
 let lastInterval = -0.05
-function pushTime()
-{
-    lastInterval+= 0.05
+function pushTime() {
+    lastInterval += 0.05
     timeData.push(lastInterval)
     positionXData.push(x)
     positionYData.push(y)
     velocityXData.push(
-        (positionXData[Math.floor(lastInterval*20)]-positionXData[Math.floor((lastInterval-0.05)*20)])/0.05
+        (positionXData[Math.floor(lastInterval * 20)] - positionXData[Math.floor((lastInterval - 0.05) * 20)]) / 0.05
     )
     velocityYData.push(
-        (positionYData[Math.floor(lastInterval*20)]-positionYData[Math.floor((lastInterval-0.05)*20)])/0.05
+        (positionYData[Math.floor(lastInterval * 20)] - positionYData[Math.floor((lastInterval - 0.05) * 20)]) / 0.05
     )
     accelerationXData.push(
-        (velocityXData[Math.floor(lastInterval*20)]-velocityXData[Math.floor((lastInterval-0.05)*20)])/0.05
+        (velocityXData[Math.floor(lastInterval * 20)] - velocityXData[Math.floor((lastInterval - 0.05) * 20)]) / 0.05
     )
     accelerationYData.push(
-        (velocityYData[Math.floor(lastInterval*20)]-velocityYData[Math.floor((lastInterval-0.05)*20)])/0.05
+        (velocityYData[Math.floor(lastInterval * 20)] - velocityYData[Math.floor((lastInterval - 0.05) * 20)]) / 0.05
     )
     posChart.update();
     velChart.update()
     accChart.update()
 }
+
+document.addEventListener("keypress", function (e) {
+    if (e.key == " ") {
+        isRecording = !isRecording
+        if (!isRecording) {
+            window.clearInterval(recordInterval)
+            return
+        }
+        recordInterval = window.setInterval(pushTime, 50)
+    }
+
+})
